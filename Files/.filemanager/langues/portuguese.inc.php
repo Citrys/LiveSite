30daa8dbc83a0619e343fab4e9b7d04995b7ee44
Dmitry Bond <m.mezon@gmail.com>
2014-11-06 21:44:15 +0200

    MashaSite

---
+++ Files/.filemanager/langues/portuguese.inc.php
@@ -0,0 +1,123 @@
+<?
+//new lang strings (3.0 version)
+$strHTMLEditableFiles 				= "Files that can be edited in HTML WYSIWYG editor";
+$strEditorBold = "Bold";
+$strEditorItalic ="Italic";
+$strEditorUnderline ="Underline";
+$strEditorStrikethrough ="Strikethrough";
+$strEditorSubscript ="Subscript";
+$strEditorSuperscript ="Superscript";
+$strEditorJustify_Left ="Justify Left";
+$strEditorJustify_Center ="Justify Center";
+$strEditorJustify_Right ="Justify Right";
+$strEditorJustify_Full ="Justify Full";
+$strEditorOrdered_List ="Ordered List";
+$strEditorBulleted_List ="Bulleted List";
+$strEditorDecrease_Indent ="Decrease Indent";
+$strEditorIncrease_Indent ="Increase Indent";
+$strEditorFont_Color ="Font Color";
+$strEditorBackground_Color ="Background Color";
+$strEditorHorizontal_Rule ="Horizontal Rule";
+$strEditorInsert_Web_Link ="Insert Web Link";
+$strEditorInsert_Image ="Insert Image";
+$strEditorInsert_Table ="Insert Table";
+$strEditorToggle_HTML_Source ="Toggle HTML Source";
+$strEditorEnlarge_Editor ="Enlarge Editor";
+$strEditorAbout_this_editor ="About this editor";
+$strEditorHelp_using_editor ="Help using editor";
+$strEditorCurrent_style ="Current style";
+
+//lang strings for versions < 3.0
+/*
+   Traduzido para portugues/Brasil
+   por Geraldo Moreira Guimarães
+   E-mail: criarte@criartebrasil.com.br
+   Home page: http://www.criartebrasil.com.br
+*/
+
+$strEditFileNorm  		   = "Editar arquivo";
+$strViewFileNorm 		   = "Ver arquivo";
+$strRestore 			   = "Restaurar original";
+$strSaveExit 			   = "Salvar e sair";
+$strEditableFiles 		   = "Arquivos editáveis";
+$strTouchBtn                       = "Criar um Arquivo";
+$strFileRepAlertChamps             = "\"É necessário o nome do arquivo !!\"";
+$strFileNewTitre                   = "Criar novo arquivo";
+$strCreerRepFileN              	   = "Criar novo arquivo no diretório: ";
+$strCreerRepNomRepFileN            = "Nome do arquivo";
+$strFileNIE               	   = "O arquivo ";
+$strErroSF 			   = "Erro ao salvar o arquivo!";
+$strCloseWin            	   = "Fechar janela";
+$strRefreshBtn                	   = "Atualizar";
+$strMkdirBtn                       = "Criar um diretório";
+$strUploadBtn                      = "Enviar arquivo";
+$strBtnStrhome                     = "Principal";
+$strBtnStrhelp                     = "Ajuda"; 
+$strBtnStrDirectory                = "Caminho: ";
+$strBtnStrNom                      = "Nome";
+$strBtnStrPrts                     = "Tamanho";
+$strBtnStrDate                     = "Data";
+$strBtnStrPermissions              = "Permissões";
+$strBtnStrActions                  = "Ações";
+$strBtnStrEffacer                  = "Apagar";
+$strBtnStrrename                   = "Renomear";
+$strBtnStrmove                     = "Mover";
+$strBtnStrcopy                     = "Copiar";
+$strBtnStrFichier                  = "O arquivo ";
+$strBtnStrRepertoire               = "O diretório ";
+$strBtnStrMsgdeleterimer           = " foi apagado.";
+$strBtnStrMsgrename                = " foi renomeado para ";
+$strBtnStrMsgCreerRep              = " foi criado.";
+$strBtnStrMsgupload                = " foi enviado.";
+$strBtnStrMsgmove                  = " foi movido para ";
+$strBtnStrMsgcopy                  = " foi copiado para ";
+$strUploadBtnSize                  = " que o tamanho é ";
+$strBtnStrAlertSD                  = "A origem e destino são idênticos!!";
+$strBtnStrAlertDeja                = " já existe!!";
+$strBtnStrErreur                   = "O sistema oparacional enviou esta mensagem de erro!!";
+$strBtnStrInfo                     = "Informação";
+$strBtnStrOptions                  = "Preferências";
+$strBtnStrPasDeFichier             = "Não há arquivo";
+$struploadTitre                    = "Enviar arquivo";
+$struploadDirectory                = "Enviar arquivo para o caminho: ";
+$struploadFichier                  = "Escolha um arquivo para enviar";
+$strupload                         = "Enviar";
+$struploadAlert                    = "\"O caminho do arquivo é necessário!!\"";
+$strCreerRepTitre                  = "Criar um diretório";
+$strCreerRepDirectory              = "Criar um diretório no caminho: ";
+$strCreerRepNomRep                 = "Nome do diretório";
+$strCreerRep                       = "Criar";
+$strCreerRepAlertChamps            = "\"É necessário um nome para o diretório!!\"";
+$strrenameTitre                    = "Enviar um arquivo";
+$strrenameOldFile                  = "Nome antigo";
+$strrenameNewFile                  = "Novo nome";
+$strrename                         = "Renomear";
+$strrenameAlert                    = "\"É necessário um nome para o novo arquivo!!\"";
+$strcopyDirectory2                 = " para o caminho:";
+$strcopy                           = "Copiar";
+$strmoveDirectory2                 = " para este caminho:";
+$strmove                           = "Mover";
+$strconfirmMessagePasVide          = " não está vazio.";
+$strconfirmMessageSure             = "Você deseja apaga-lo?";
+$strconfirmMessageFichier          = "Você deseja apagar o arquivo ";
+$strconfirmMessageRep              = "Você deseja apagar o diretório ";
+$strAnnuler                        = "Cancelar";
+$strConfirmer                      = "Aceitar";
+$strAlertChar                      = "\"caracteres errados!! \\nNão use os seguintes caracteres: \\n\\\ / : * ? \\'\\' < > | \"";
+$strValider                        = "Aceitar";
+$strOptionTitre			   = "Preferências";
+$strOptionLangue		   = "Linguagem";
+$strOptionDiversOptions		   = "Opções variadas";
+$strmoveTitre                      = "Mover um arquivo ou um diretório";
+$strmoveDirectory1                 = "Mover ";
+$strcopyTitre                      = "Copiar um arquivo ou um diretório";
+$strcopyDirectory1                 = "Copiar  ";
+$strOptionRelDirN		   = "Aponte o caminho relativo<BR>entre o aplicativo e o diretório raiz do Site";
+$strBtnStrDownload		   = "Download";
+$srtAproposFichier		   = "Arquivo";
+$srtAproposFichiers		   = "Arquivos";
+$srtAproposRepertoire		   = "Diretório";
+$srtAproposRepertoires		   = "Diretórios";
+
+?>
+
